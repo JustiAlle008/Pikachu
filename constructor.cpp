7a0b2b7b663f1eacd2e1cf016fe95970461bf5a6
JustiAlle008 <112235201+JustiAlle008@users.noreply.github.com>
2022-08-27 13:18:49 +0530

    Add files via upload

---
+++ constructor.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class bass_class{
+	public:
+		bass_class()
+		{
+			cout<<"constructor is executed"<<"\n";
+			
+		}
+		
+};
+int main()
+{
+	bass_class ob1;
+	bass_class ob2;
+}
